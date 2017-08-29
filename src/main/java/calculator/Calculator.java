344f1713cd30e417f37246fa7e93760d895566f7
mes32 <stockman.mike@gmail.com>
2017-08-28 23:54:14 -0400

    setup directory structure and gradle

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,15 @@
+/*
+    Calculator.java
+
+    Launching class for calculator application
+ */
+
+package calculator;
+
+import java.io.*;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("... In Calculator.java");
+    }
+}
