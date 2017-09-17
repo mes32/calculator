36752f4b997d294e5dfbf861a60a47c70b8017f8
mes32 <stockman.mike@gmail.com>
2017-09-17 01:16:52 -0400

    feature: Finish wiring up event handlers in CalculatorController.

--- src/main/java/calculator/CalculatorController.java
+++ src/main/java/calculator/CalculatorController.java
@@ -18,6 +18,23 @@ public class CalculatorController {
     private void addListeners(ButtonSet button) {
         button.clear().addActionListener(event->pressClear());
         button.equals().addActionListener(event->pressEquals());
+        button.percent().addActionListener(event->pressPercent());
+        button.inverse().addActionListener(event->pressInverse());
+        button.decimal().addActionListener(event->pressDecimal());
+        button.add().addActionListener(event->pressAdd());
+        button.sub().addActionListener(event->pressSub());
+        button.mul().addActionListener(event->pressMul());
+        button.div().addActionListener(event->pressDiv());
+        button.num0().addActionListener(event->pressNum0());
+        button.num1().addActionListener(event->pressNum1());
+        button.num2().addActionListener(event->pressNum2());
+        button.num3().addActionListener(event->pressNum3());
+        button.num4().addActionListener(event->pressNum4());
+        button.num5().addActionListener(event->pressNum5());
+        button.num6().addActionListener(event->pressNum6());
+        button.num7().addActionListener(event->pressNum7());
+        button.num8().addActionListener(event->pressNum8());
+        button.num9().addActionListener(event->pressNum9());
     }
 
     private void pressClear() {
@@ -27,6 +44,91 @@ public class CalculatorController {
 
     private void pressEquals() {
         System.out.println("pressEquals()");
+        //view.update();
+    }
+
+    private void pressPercent() {
+        System.out.println("pressPercent()");
+        //view.update();
+    }
+
+    private void pressInverse() {
+        System.out.println("pressInverse()");
+        //view.update();
+    }
+
+    private void pressDecimal() {
+        System.out.println("pressDecimal()");
+        //view.update();
+    }
+
+    private void pressAdd() {
+        System.out.println("pressAdd()");
+        //view.update();
+    }
+
+    private void pressSub() {
+        System.out.println("pressSub()");
+        //view.update();
+    }
+
+    private void pressMul() {
+        System.out.println("pressMul()");
+        //view.update();
+    }
+
+    private void pressDiv() {
+        System.out.println("pressDiv()");
+        //view.update();
+    }
+
+    private void pressNum0() {
+        System.out.println("pressNum0()");
+        //view.update();
+    }
+
+    private void pressNum1() {
+        System.out.println("pressNum1()");
+        //view.update();
+    }
+
+    private void pressNum2() {
+        System.out.println("pressNum2()");
+        //view.update();
+    }
+
+    private void pressNum3() {
+        System.out.println("pressNum3()");
+        //view.update();
+    }
+
+    private void pressNum4() {
+        System.out.println("pressNum4()");
+        //view.update();
+    }
+
+    private void pressNum5() {
+        System.out.println("pressNum5()");
+        //view.update();
+    }
+
+    private void pressNum6() {
+        System.out.println("pressNum6()");
+        //view.update();
+    }
+
+    private void pressNum7() {
+        System.out.println("pressNum7()");
+        //view.update();
+    }
+
+    private void pressNum8() {
+        System.out.println("pressNum8()");
+        //view.update();
+    }
+
+    private void pressNum9() {
+        System.out.println("pressNum9()");
         //view.update();
     }
 }
